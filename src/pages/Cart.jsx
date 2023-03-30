313aef213c6415355ad46dd473a6805d2b17df6b
rivabayu <rivabays@gmail.com>
2023-03-30 15:54:26 +0700

    redux Slice

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Helmet from '../component/Helmet/Helmet'
 import CommonSection from '../component/UI/CommonSection'
-import img from '../assets/images/double-sofa-01.png'
 import { MdDeleteOutline } from 'react-icons/md'
 import { cartActions } from '../redux/slices/cartSlice'
 import { useSelector, useDispatch } from 'react-redux'
@@ -16,7 +15,7 @@ const Cart = () => {
       <section className='mx-20 lg:mx-40 mt-10'>
         <div className=''>
           {
-            cartItems.length === 0 ? (<h2 className='text-xl font-semibold'>No Item added to cart</h2>) 
+            cartItems?.length === 0 ? (<h2 className='text-xl font-semibold'>No Item added to cart</h2>) 
             :(
               <div className='lg:w-[70rem] gap-10'>
               <div className='border-b-4 flex text-lg font-bold '>
@@ -28,16 +27,8 @@ const Cart = () => {
               </div>
               <div>
                 {
-                  cartItems.map((item, index) =>(
-                    <div key={index} className='flex border-b-2'>
-                      <span className='w-[25rem] flex justify-start'>
-                        <img src={item.imgUrl} className='w-36 pr-5 flex' alt="" />
-                      </span>
-                      <span className='w-[20rem]  py-4'>{item.productName}</span>
-                      <span className='w-[20rem] text-end py-4' >$ {item.price}</span>
-                      <span className='w-[20rem] text-end py-4'>{item.quantity} px</span>
-                      <span className='w-[20rem] text-end py-4 flex justify-end'><MdDeleteOutline className='text-2xl'/></span>
-                    </div>
+                  cartItems?.map((item, index) =>(
+                   <Tr item={item} key={index}/>
                   ))
                 }
               </div>
@@ -46,11 +37,38 @@ const Cart = () => {
             )
           }
          
-          <div>2</div>
+          <div></div>
         </div>
       </section>
     </Helmet>
   )
+}
+
+const Tr = ({item}) =>{
+ const dispatch = useDispatch()
+ 
+const deleteProduct =() =>{
+  dispatch(cartActions.deleteItem(item?.id))
+  console.log("itemssss",item);
+}
+
+  return <>
+  <div  className='flex border-b-2'> 
+    <span className='w-[25rem] flex justify-start'>
+     <img src={item.imgUrl} className='w-36 pr-5 flex' alt="" />
+    </span>
+    <span className='w-[20rem]  py-4'>{item.productName}</span>
+    <span className='w-[20rem] text-end py-4' >$ {item.price}</span>
+    <span className='w-[20rem] text-end py-4'>{item.quantity} px</span>
+    <span className='w-[20rem] text-end py-4 flex justify-end'>
+      <span className='btn btn-ghost' >
+        <MdDeleteOutline onClick={deleteProduct} 
+           className='text-2xl  text-black  '/>
+       </span>
+    </span>
+  </div>
+  </>
+  
 }
 
 export default Cart
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
+import Helmet from '../component/Helmet/Helmet'
+import CommonSection from '../component/UI/CommonSection'
 
+import { useSelector } from 'react-redux'
 const Checkout = () => {
+
+  const totalQty = useSelector(state => state.cart.totalQuantity)
+  const totalAmount = useSelector(state => state.cart.totalAmount)
   return (
-    <div>
-      Checkout
-    </div>
+    <Helmet title='Checkout' >
+      <CommonSection title="Checkout"/>
+
+      <section className='mx-20 lg:mx-40 mt-10'>
+        <div className='flex lg:flex-row md:flex-col'>
+          <div className=''>
+              <div className='text-xl font-semibold'>Biling Information</div>
+              <div className='lg:w-[40rem] '>
+                <input type="text" placeholder="Your Name" className="input input-bordered w-full my-2 " />
+                <input type="text" placeholder="Your Email" className="input input-bordered w-full my-2" />
+                <input type="text" placeholder="Phone Number" className="input input-bordered w-full my-2" />
+                <input type="text" placeholder="Street Address" className="input input-bordered w-full my-2" />
+                <input type="text" placeholder="City" className="input input-bordered w-full my-2" />
+                <input type="text" placeholder="Postal Code" className="input input-bordered w-full my-2" />
+                <input type="text" placeholder="Country" className="input input-bordered w-full my-2" />
+              </div>
+          </div>
+          <div>
+            <div className='bg-headingText text-white p-2 mt-5 rounded-lg lg:mx-20 w-[20rem] lg:h-[13rem]'>
+                <div className='pt-2 px-4 flex justify-between'>
+                  <span>Total Qty</span>
+                  <span className='font-semibold'>{totalQty} items</span>
+                </div>
+                <div className='pt-2 px-4 flex justify-between'>
+                  <span>Subtotal</span>
+                  <span className='font-semibold'>$ {totalAmount}</span>
+                </div>
+                <div className='pt-2 px-4 flex justify-between'>
+                  <span>Shipping</span>
+                  <span className='font-semibold'>10</span>
+                </div>
+                <div className='pt-2 pb-2 px-4 flex justify-between '>
+                  <span>Free Shipping</span>
+                  <span className='font-semibold'>10</span>
+                </div>
+                <div className='pt-2 flex justify-between border-t-[1px] mx-4'>
+                  <span className='text-2xl font-bold'>Total Cost</span>
+                  <span className='font-bold text-2xl'>$ {totalAmount}</span>
+                </div>
+                <div className='flex justify-center'>
+                  <button className="btn btn-wide mt-2 flex bg-white text-headingText hover:bg-headingText hover:text-white hover:border-none">Order</button>
+                </div>
+            </div>
+           
+          </div>
+        </div>
+      </section>
+    </Helmet>
+    
+   
   )
 }
 
