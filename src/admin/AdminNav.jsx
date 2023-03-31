80374fcf3022a84b2a4c965fd62f391185ddd4a2
rivabayu <rivabays@gmail.com>
2023-03-31 09:50:28 +0700

    dasboard admin

---
+++ src/admin/AdminNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminNav = () => {
+  return (
+    <div>
+      Admin Nav
+    </div>
+  )
+}
+
+export default AdminNav
---
+++ src/admin/AllProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllProduct = () => {
+  return (
+    <div>
+      Allproduct
+    </div>
+  )
+}
+
+export default AllProduct
