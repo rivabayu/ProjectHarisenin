3b22164d8f37e785055cb3149ce774ef606f6cef
Riva Bayu Syarifudin <117695139+rivabayu@users.noreply.github.com>
2023-04-01 20:28:18 +0700

    Update Helmet.jsx

--- src/component/Helmet/Helmet.jsx
+++ src/component/Helmet/Helmet.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Helmet = (props) => {
-    document.title = "ID Barkas - " + props.title;
+    document.title = "ID Store - " + props.title;
   return <div className='w-100'>
       {props.children}
     </div>
